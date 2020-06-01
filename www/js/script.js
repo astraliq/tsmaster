e3dd928e11184e9b8229da747f073bfe862a0259
Leonid <leoural66@yandex.ru>
2020-06-01 21:48:45 +0500

    Запустил пагинацию, добавли заглушки
    Запустил пагинацию, добавли заглушки в index

--- www/js/script.js
+++ www/js/script.js
@@ -1,14 +1,11 @@
-'use strict';
+'use strict'
 
 //Карусель верхнего модуля
 //помечаем код для удобства
 let i = 1;
 for (let li of carousel.querySelectorAll('li')) {
     li.style.position = 'relative';
-    li.insertAdjacentHTML(
-        'beforeend',
-        '<span style="position:absolute;left:0;top:0">${i}</span>'
-    );
+    li.insertAdjacentHTML('beforeend', '<span style="position:absolute;left:0;top:0">${i}</span>');
     i++;
 }
 
@@ -20,16 +17,29 @@ let numPoint = 0; //текущая картинка, точка верхнего
 
 let list = carousel.querySelector('ul');
 let listElems = carousel.querySelectorAll('li');
+let point = points.querySelectorAll('a');
 
-let position = 0; //положение прокрутки
+var position = 0; //положение прокрутки
+
+//переход по точкам верхнего слайдера
+point.forEach(element => {
+    element.addEventListener('click', number => {
+        let pointData = number.srcElement.dataset.point;
+        position = -1 * width * pointData;//меняем позицию слайдера согласно точке
+        list.style.marginLeft = position + 'px'; //обращение к стилю тэга ul         
+        pointActive(pointData);
+
+    });
+});
 
 //сдвиг в лево Размеры зависят от картинок в верстке
 carousel.querySelector('.prev').onclick = function () {
+
     position += width * count;
     if (position > 0) position = -1 * (width * (numberImg - count)); //строка возвращает на начало списка
 
     position = Math.min(position, 0);
-    list.style.marginLeft = position + 'px'; //обращение у стилю тэга ul
+    list.style.marginLeft = position + 'px'; //обращение у стилю тэга ul    
 
     numPoint--; //данные для точек
     if (numPoint < 0) numPoint = numberImg - 1;
@@ -38,26 +48,27 @@ carousel.querySelector('.prev').onclick = function () {
 
 //сдвиг в право Размеры зависят от картинок в верстке
 carousel.querySelector('.next').onclick = function () {
+
     position -= width * count;
     if (position <= -1 * (width * numberImg)) position = 0; // строка перемещает на конец списка
 
     position = Math.max(position, -width * (listElems.length - count));
     list.style.marginLeft = position + 'px'; //обращение у стилю тэга ul
 
+
     numPoint++; //данные для точек
     if (numPoint > 4) numPoint = 0;
     pointActive(numPoint);
 };
 //Делаем зависимость точек
-let point = points.querySelectorAll('a');
-
 //подстветка точек
 function pointActive(num) {
-    point.forEach((elem) => {
+    point.forEach(elem => {
         elem.classList.remove('slider__point_active');
     });
     point[num].classList.add('slider__point_active');
-}
+};
+
 
 //курусель отзывов
 /*******************************************************************************/
