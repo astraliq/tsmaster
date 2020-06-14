e8d975fe5e903247116185047a403571f3d9cc01
Leonid <leoural66@yandex.ru>
2020-06-14 15:48:06 +0500

    Добавление выбора бренда
    Добавлен выбор бренда в URL, сделана простая проверка URL, но при выборе бренда ниже 2-го уровня появляется ошибка. Расширить проверку на 3-й уровень. Карусель брендов перенастроена.

--- www/js/brand.js
+++ www/js/brand.js
@@ -6,6 +6,7 @@ class Brand {
         this.urlDataBrand = '../js/brands.json';
         this.blockBrand = '';
         this.ulBrand = document.querySelector('.image_br');
+        this.nameTechnic = '';
     }
 
     async _getDataBrand(url) {
@@ -15,11 +16,35 @@ class Brand {
         console.log(this.dataBrand);
     }
 
+    //получаем и обрабатываем URL по брендам
+    getUrlTechnic() {
+        let url = new URL(document.URL);
+        let arrUrl = [...url.pathname];
+        let removElem = arrUrl.splice(0, 1);
+        let name = arrUrl.join('');
+
+        if (name == 'washing_machine' || name == 'dishwasher' || name == 'refrigerator' || name == 'oven' || name == 'hob') {
+            this.nameTechnic = name;
+        } else {
+            this.checkUrlTechnic(name);
+        }
+
+        console.log(this.nameTechnic);
+    }
+
+    //проверяем URL на выбранный бренд
+    checkUrlTechnic(nameUrl) {
+        let numberElement = nameUrl.indexOf('_');
+        let arrRemove = [...nameUrl];
+        let removeEl = arrRemove.splice(0, numberElement + 1);
+        this.nameTechnic = arrRemove.join('');
+    }
+
     createBlock() {
         for (let i = 0; i < this.dataBrand.length; i++) {
             this.blockBrand = this.blockBrand + `
                 <li>
-                    <a href="#" class="gallery_br_link" style="                    
+                    <a href="/${this.dataBrand[i].brandName}_${this.nameTechnic}" class="gallery_br_link" style="                    
                             background-image: url(../img/brands/${this.dataBrand[i].imgBrand}); 
                             background-size: contain;
                             background-repeat: no-repeat;
@@ -34,6 +59,7 @@ class Brand {
 
     init() {
         this._getDataBrand(this.urlDataBrand);
+        this.getUrlTechnic();
     }
 }
 
@@ -46,14 +72,13 @@ setTimeout(() => {
     let k = 1;
     for (let li of carousel_br.querySelectorAll('li')) {
         li.style.position = 'relative';
-        li.insertAdjacentHTML('beforeend', '<span style="position:absolute;left:0;top:0">${i}</span>'
-        );
+        li.insertAdjacentHTML('beforeend', '<span style="position:absolute;left:0;top:0">${i}</span>');
         k++;
     };
 
 
     //конфигурация
-    let width_br = 335; //ширина картинки 
+    let width_br = 250; //ширина картинки 
     let count_br = 3; //сдиг на количество картинок
     let numberImg_br = brand.dataBrand.length; //количество картинок
 
